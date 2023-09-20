3528ef93e8aa3e8264c0b173973120e58fc10aa0
Sonia-saini <105914181+Sonia-saini@users.noreply.github.com>
2023-09-20 16:27:43 +0530

    assignment 2 done

--- Assignment 2/RouteAlogrithm.js
+++ Assignment 2/RouteAlogrithm.js
@@ -1,39 +1,49 @@
 const availabletickets = {
-    Paris: 'Skopje',
-    Zurich: 'Amsterdam',
-    Prague: 'Zurich',
-    Barcelona: 'Berlin',
-    Kiev: 'Prague',
-    Skopje: 'Paris',
-    Amsterdam: 'Barcelona',
-    Berlin: 'Kiev',
-    Berlin: 'Amsterdam',
-  };
-  // Function to find the travel route
-  function findTravelRoute(startingCity, availabletickets) {
-    const route = [startingCity]; 
-    let currentCity = startingCity;
-  
-    while (true) {
-      // Get the next city from the tickets availbility
-      const nextCity = availabletickets[currentCity];
-  
-      // If there's no next city or if we've already visited it, break the loop
-      if (!nextCity || route.includes(nextCity)) {
-        break;
-      }
-  
-      // Add the next city to the route and update the current city
-      route.push(nextCity);
-      currentCity = nextCity;
+  Paris: "Skopje",
+  Zurich: "Amsterdam",
+  Prague: "Zurich",
+  Barcelona: "Berlin",
+  Kiev: "Prague",
+  Skopje: "Paris",
+  Amsterdam: "Barcelona",
+  Berlin: "Kiev",
+  Berlin: "Amsterdam",
+};
+let cityvisited = {
+  Amsterdam: false,
+  Kiev: false,
+  Zurich: false,
+  Prague: false,
+  Berlin: false,
+  Barcelona: false,
+};
+// Function to find the travel route
+function findTravelRoute(startingCity, availabletickets) {
+  const route = [startingCity];
+  let currentCity = startingCity;
+  let count = 0;
+  while (count < 6) {
+    count++;
+    // Get the next city from the tickets availbility
+    const nextCity = availabletickets[currentCity];
+
+    // If there's no next city or if we've already visited it, break the loop
+    if (!nextCity || route.includes(nextCity)) {
+      break;
     }
-  
-    return route;
+
+    // Add the next city to the route and update the current city
+    route.push(nextCity);
+    currentCity = nextCity;
+    cityvisited[currentCity] = true;
   }
-  const startingCity = 'Kiev';
-  const travelRoute = findTravelRoute(startingCity, availabletickets);
-  
-  console.log('Travel Route:', travelRoute);
+
+  return route;
+}
+const startingCity = "Kiev";
+const travelRoute = findTravelRoute(startingCity, availabletickets);
+
+console.log("Travel Route:", travelRoute);
 //   Assignment 2: Using JavaScript
 //   Q . Your son took a vacation through Europe without telling you. When the kid returned from the vacation you asked him where did he go. The kid told you: Dad I went to these cities: Amsterdam, Kiev, Zurich, Prague, Berlin, Barcelona.
 //   I used only train as transportation and these were the available tickets:
